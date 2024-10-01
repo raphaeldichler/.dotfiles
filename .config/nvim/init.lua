cee0605c912a0967ddcf7c446de3287224b03573
Raphael Dichler <44930718+raphaeldichler@users.noreply.github.com>
2024-10-01 23:59:55 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,22 @@
-require("private")
+-- Create an autocommand that triggers when opening a new .java file
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.java",
+    callback = function()
+        local dir = vim.fn.expand('%:p:h')
+        local package_name = dir:gsub("/", "."):gsub("^.*src%.main%.java%.", "")
 
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+            "package " .. package_name .. ";",
+            "",
+            "/**",
+            " *",
+            " * @author Raphael Dichler",
+            " */",
+            "public class " .. vim.fn.expand("%:t:r") .. " {",
+            "",
+            "}"
+        })
+    end,
+})
+
+require("private")
