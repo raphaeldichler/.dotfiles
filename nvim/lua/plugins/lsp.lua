52a37ed181e82a59e7f77398738b3943b310f019
Raphael Dichler <44930718+raphaeldichler@users.noreply.github.com>
2025-03-06 14:33:49 +0100

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,8 +15,8 @@ return {
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       require("lspconfig").lua_ls.setup { capabilites = capabilities }
-
-      require("lspconfig").gopls.setup {}
+      require("lspconfig").gopls.setup { capabilites = capabilities }
+      require("lspconfig").pyright.setup { capabilites = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
