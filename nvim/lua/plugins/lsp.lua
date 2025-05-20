c15feda7ebf679456a50f856f49f761705692dfe
Raphael Dichler <44930718+raphaeldichler@users.noreply.github.com>
2025-05-20 12:25:49 +0200

    Update; add rename

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,6 +18,8 @@ return {
       require("lspconfig").gopls.setup { capabilites = capabilities }
       require("lspconfig").pyright.setup { capabilites = capabilities }
 
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { desc = "[r]e[n]ame symbol" })
+      
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local c = vim.lsp.get_client_by_id(args.data.client_id)
