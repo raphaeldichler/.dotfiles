0f6f9bd0010a78ffb1ca04ff67aeef183dd5fd3d
Raphael Dichler <44930718+raphaeldichler@users.noreply.github.com>
2025-06-09 22:19:41 +0200

    add cland, protobuf lsp and golang auto format on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,11 +15,31 @@ return {
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       require("lspconfig").lua_ls.setup { capabilites = capabilities }
-      require("lspconfig").gopls.setup { capabilites = capabilities }
-      require("lspconfig").pyright.setup { capabilites = capabilities }
-
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { desc = "[r]e[n]ame symbol" })
       
+      require('lspconfig').gopls.setup {
+        capabilites = capabilities,
+        on_attach = function(client, bufnr)
+          if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
+      }
+
+      require("lspconfig").pyright.setup { capabilites = capabilities }
+      require('lspconfig').buf_ls.setup {}
+
+      require('lspconfig').clangd.setup({
+        cmd = { "clangd-18" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+      })
+      
+      vim.api.nvim_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local c = vim.lsp.get_client_by_id(args.data.client_id)
